70c7c650a2ad6f8eca26c72490cd7429f06298df
Vrosko <41743548+Vrosko@users.noreply.github.com>
2018-07-26 15:09:10 +0200

    Add files via upload

---
+++ Experiment Mk. 1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace Experiment_Mk._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Calculator:
+            try
+            { 
+            string input = Console.ReadLine();          
+            
+                int[] Numbers = { 1, 2 };
+                string[] StringSplitter()
+                {
+                    char[] delimiterChars = { '-', '+', '*', '/' };
+                    string data = input;
+                    string[] factors = data.Split(delimiterChars);
+                return factors;
+                }
+                if (input.Contains('*'))
+                {
+                    var Factor = StringSplitter();
+                    int Calc1 = Convert.ToInt32(Factor[0]);
+                    int Calc2 = Convert.ToInt32(Factor[1]);
+                    int awnser = Calc1 * Calc2;
+                    Console.WriteLine(awnser);
+                }
+                else if (input.Contains('/'))
+                {
+                    var Factor = StringSplitter();
+                    int Calc1 = Convert.ToInt32(Factor[0]);
+                    int Calc2 = Convert.ToInt32(Factor[1]);
+                    int awnser = Calc1 / Calc2;
+                    Console.WriteLine(awnser);
+                }
+                else if (input.Contains('+'))
+                {
+                    var Factor = StringSplitter();
+                    int Calc1 = Convert.ToInt32(Factor[0]);
+                    int Calc2 = Convert.ToInt32(Factor[1]);
+                    int awnser = Calc1 + Calc2;
+                    Console.WriteLine(awnser);
+                }
+                else if (input.Contains('-'))
+                {
+                    var Factor = StringSplitter();
+                    int Calc1 = Convert.ToInt32(Factor[0]);
+                    int Calc2 = Convert.ToInt32(Factor[1]);
+                    int awnser = Calc1 - Calc2;
+                    Console.WriteLine(awnser);
+                }
+                else
+                {
+                    Console.WriteLine("That's no equation...");
+                }
+            }        
+            
+            catch(Exception)
+            {
+                Console.WriteLine("That's no equation...");
+            }
+            goto Calculator;
+        }
+    }
+}
+
+
+
+
+//int input = Int32.Parse(Console.ReadLine());
+//int input2 = Int32.Parse(Console.ReadLine());
+//int output = input + input2;
+//Console.WriteLine(output);
+//Console.ReadLine();
